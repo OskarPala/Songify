b6347c3858fac9ffc83d066dd125576fd0a9d432
Oskar <oskar.pala92@gmail.com>
2024-07-18 18:26:49 +0200

    refactored PatchMapping

--- src/main/java/com/songify/song/domain/service/SongUpdater.java
+++ src/main/java/com/songify/song/domain/service/SongUpdater.java
@@ -20,6 +20,24 @@ public class SongUpdater {
 
     public void updateById(Long id, Song newSong) {
         songRetriever.existsById(id);
-        songRepository.updateById(id,newSong);
+        songRepository.updateById(id, newSong);
+    }
+
+    public Song updatePartiallyById(Long id, Song songFromRequest) {
+        Song songFromDatabase = songRetriever.findSongById(id);
+        Song.SongBuilder builder = Song.builder();
+        if (songFromRequest.getName() != null) {
+            builder.name(songFromRequest.getName());
+        } else {
+            builder.name(songFromDatabase.getName());
+        }
+        if (songFromRequest.getArtist() != null) {
+            builder.artist(songFromRequest.getArtist());
+        } else {
+            builder.artist(songFromDatabase.getArtist());
+        }
+        Song toSave = builder.build();
+        updateById(id, toSave);
+        return toSave;
     }
 }

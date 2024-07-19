afc3b78ca1d1f9aeab262d910f6e8cbec959e20b
Oskar <oskar.pala92@gmail.com>
2024-07-19 19:25:31 +0200

    dirty checking version added

--- src/main/java/com/songify/song/domain/service/SongUpdater.java
+++ src/main/java/com/songify/song/domain/service/SongUpdater.java
@@ -41,3 +41,21 @@ public class SongUpdater {
         return toSave;
     }
 }
+
+//Dirty checking version
+//public void updateById(Long id, Song newSong) {
+//    Song songById = songRetriever.findSongById(id);
+//    songById.setName(newSong.getName());
+//    songById.setArtist(newSong.getArtist());
+//}
+//
+//public Song updatePartiallyById(Long id, Song songFromRequest) {
+//    Song songFromDatabase = songRetriever.findSongById(id);
+//    if (songFromRequest.getName() != null) {
+//        songFromDatabase.setName(songFromRequest.getName());
+//    }
+//    if (songFromRequest.getArtist() != null) {
+//        songFromDatabase.setArtist(songFromRequest.getArtist());
+//    }
+//    return songFromDatabase;
+//}

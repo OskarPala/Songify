97aee61c460bdd9f8539cee10f147e0adf37c5a7
Oskar <oskar.pala92@gmail.com>
2024-09-16 18:57:46 +0200

    added integration packages, testcontainers and spring data connection to postgresql

--- src/test/java/com/songify/domain/crud/SongifyCrudFacadeTest.java
+++ src/test/java/com/songify/domain/crud/SongifyCrudFacadeTest.java
@@ -364,4 +364,5 @@ class SongifyCrudFacadeTest {
         assertThat(songifyCrudFacade.findAllAlbums()).isEmpty();
         assertThat(songifyCrudFacade.findAllSongs(Pageable.unpaged())).isEmpty();
     }
+
 }

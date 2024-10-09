8eb5d22c64b745b5b66ec5636902c27ae1575db6
Oskar <oskar.pala92@gmail.com>
2024-10-09 22:21:25 +0200

    added uuid, created_on, version to table users, and added security user with userdetails, and userdetailsserviceimpl

---
+++ src/main/java/com/songify/infrastructure/security/SecurityUser.java
@@ -0,0 +1,55 @@
+package com.songify.infrastructure.security;
+
+import com.songify.domain.usercrud.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+class SecurityUser implements UserDetails {
+    private final User user;
+
+    SecurityUser(final User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities()
+                .stream()
+                .map(authority -> (GrantedAuthority) () -> authority)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}

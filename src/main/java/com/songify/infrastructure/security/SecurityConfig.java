2aa86d3893e9366a87ff252b83ae210524906c42
Oskar <oskar.pala92@gmail.com>
2024-10-10 21:50:47 +0200

    added bcrypt

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.songify.domain.usercrud.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +17,6 @@ class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/resources/db.migration/general/V20__insert_users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (email, password, authorities, enabled)
-VALUES
-    ('oskar', '12345', '{ROLE_ADMIN, ROLE_USER}', true),
-    ('john', '12345', '{ROLE_USER}', true);
